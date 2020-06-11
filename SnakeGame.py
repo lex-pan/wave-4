47ea2ec87b97afcfaefc4dc4059fa56c41c419bd
Ryan <whiplash5000a@gmail.com>
2020-06-11 16:06:38 -0400

    first commit

---
+++ SnakeGame.py
@@ -0,0 +1,10 @@
+import turtle
+
+#Setting up the screen
+window = turtle.Screen()
+window.title("Snake Game by Lexiang Pan and Ryan Shen")
+window.bgcolor("green")
+window.setup(width=600, height = 600)
+window.tracer(0)
+
+window.mainloop()
